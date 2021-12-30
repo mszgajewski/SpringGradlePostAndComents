2fab23902782364485a9aca4acbbf82d1de639c1
mszgajewski <mszgajewski@gmail.com>
2021-12-30 20:15:04 +0100

    Initial commit

---
+++ src/main/java/com/example/SpringGradlePostAndComents/SpringGradlePostAndComentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringGradlePostAndComents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGradlePostAndComentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGradlePostAndComentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringGradlePostAndComents/SpringGradlePostAndComentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringGradlePostAndComents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGradlePostAndComentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
